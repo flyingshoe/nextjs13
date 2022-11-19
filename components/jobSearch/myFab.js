780e39e92b5324a9afbffc331d412d5bdbb4137f
Leo <goldcarrotminer@gmail.com>
2022-11-20 02:37:58 +0800

    fix: shift fab slightly to the right

--- components/jobSearch/myFab.js
+++ components/jobSearch/myFab.js
@@ -2,7 +2,7 @@ import { Fab } from "@mui/material";
 
 export default function MyFab({ children, ...props }) {
   return (
-    <Fab sx={{ position: "fixed", bottom: 30, right: 30 }} {...props}>
+    <Fab sx={{ position: "fixed", bottom: 30, right: 15 }} {...props}>
       {children}
     </Fab>
   );
