7fa161c4d022915893ef3660fb22011b24f74240
Leo <goldcarrotminer@gmail.com>
2022-11-19 16:47:32 +0800

    feat: Add loading animaiton for route changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,29 @@
+import { LinearProgress } from "@mui/material";
+import Router from "next/router";
+import { useEffect, useState } from "react";
 import Navbar from "../components/jobSearch/navbar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    Router.events.on("routeChangeStart", (url) => {
+      setLoading(true);
+    });
+
+    Router.events.on("routeChangeComplete", (url) => {
+      setLoading(false);
+    });
+
+    Router.events.on("routeChangeError", (url) => {
+      setLoading(false);
+    });
+  }, []);
   return (
     <>
       <Navbar />
+      {loading && <LinearProgress />}
       <Component {...pageProps} />
     </>
   );
