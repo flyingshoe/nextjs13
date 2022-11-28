93f3673c743e5354f1bb5a873e6e408e14471fd9
Leo <goldcarrotminer@gmail.com>
2022-11-29 04:53:07 +0800

    feat: dynamically show FabGroup expand icon depending on screen size

--- components/jobSearch/FabContainer.js
+++ components/jobSearch/FabContainer.js
@@ -1,18 +1,45 @@
 import { Fab } from "@mui/material";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Box, Grow } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { CSSTransition } from "react-transition-group";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function FabContainer({ children, ...props }) {
   const [isOpen, setIsOpen] = useState(false);
   const isGrp = Array.isArray(children);
   const nodeRef = useRef(null);
+  const theme = useTheme();
+  const bigScreen = useMediaQuery(theme.breakpoints.up("md"));
 
   const toggleFab = () => {
     setIsOpen(!isOpen);
   };
 
+  // Screen is MD and up
+  if (bigScreen) {
+    return (
+      <Box
+        sx={{
+          position: "fixed",
+          bottom: 30,
+          right: 15,
+          display: "flex",
+          flexDirection: "column",
+          gap: 3,
+        }}
+      >
+        {children.map((child, index) => (
+          <Fab key={index} color="primary">
+            {child}
+          </Fab>
+        ))}
+      </Box>
+    );
+  }
+
+  // Mobile devices (xs,sm)
   return (
     <Box
       sx={{
@@ -30,7 +57,7 @@ export default function FabContainer({ children, ...props }) {
       ) : (
         <>
           {/* FAB Group */}
-          <Grow in={isOpen} timeout={300} style={{ transformOrigin: "bottom" }}>
+          <Grow in={isOpen} timeout={400} style={{ transformOrigin: "bottom" }}>
             <Box
               sx={{
                 display: "flex",
