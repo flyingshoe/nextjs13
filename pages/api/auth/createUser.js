5508aba26ca06ea9b6330829c804bf7560ca219f
Leo <goldcarrotminer@gmail.com>
2022-12-11 19:02:24 +0800

    fix: refactor createUser API

--- pages/api/auth/createUser.js
+++ pages/api/auth/createUser.js
@@ -2,16 +2,22 @@ const fs = require("fs");
 let users = require("/data/users.json");
 
 export default async (req, res) => {
-  console.log(req);
   if (req.method === "PUT") {
     const { username, password } = req.body.data;
-    users[username] = password;
 
-    saveData();
-    res.status(200).json({ success: true });
+    // User already exists
+    if (username in users) {
+      res.status(200).json({ success: false, msg: "User already exists" });
+    }
+    // New user, add to users object and save to DB
+    else {
+      users[username] = password;
+      saveData();
+      res.status(200).json({ success: true });
+    }
   }
 };
 
 function saveData() {
-  fs.writeFileSync("data/users.json", JSON.stringify(users, null, 4));
+  fs.writeFileSync("data/users.json", JSON.stringify(users, null, 2));
 }
